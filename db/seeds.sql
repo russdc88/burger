1deb451a53187a5226c94f74e0c157a45a826fc9
russdc88 <russdc88@gmail.com>
2018-11-06 14:00:30 -0700

    final push

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pastrami Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,46 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-eaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEaten = true;
+
+    var newEatenState = {
+      eaten: newEaten
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newEatenState
+    }).then(
+      function() {
+        console.log("changed eaten to", newEaten);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#bu").val().trim(),
+      sleepy: false
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger!");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+});
