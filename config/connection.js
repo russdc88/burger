ba5dfb745e01f30d3bc1bbcdf395172a9a386341
russdc88 <russdc88@gmail.com>
2018-11-06 15:08:26 -0700

    removed jaws

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 var mysql = require("mysql");
 var connection
 
-if(process.env.JAWSDB_URL){
-	connection = mysql.createConnection(process.env.JAWSDB_URL)
-}
-else {
+// if(process.env.JAWSDB_URL){
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL)
+// }
+// else {
 
 	
 	connection = mysql.createConnection({
@@ -15,7 +15,7 @@ else {
 		password: "wildcard88",
 		database: "burgers_db"
 	});
-}
+
 
 // Make connection.
 connection.connect(function(err) {
