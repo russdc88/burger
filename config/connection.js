1dad8cc3a2e7b180e12e7ac6342fbb4f4ceef98a
russdc88 <russdc88@gmail.com>
2018-11-06 15:28:42 -0700

    adding back jaws

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 var mysql = require("mysql");
 var connection
 
-// if(process.env.JAWSDB_URL){
-// 	connection = mysql.createConnection(process.env.JAWSDB_URL)
-// }
-// else {
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
 
 	
 	connection = mysql.createConnection({
@@ -15,7 +15,7 @@ var connection
 		password: "wildcard88",
 		database: "burgers_db"
 	});
-
+}
 
 // Make connection.
 connection.connect(function(err) {
